1e3aa3b0049b7f28207d4bd648f9dcf4585c3b44
Ryan H Lewis <ryanhhlewis@gmail.com>
2021-03-06 17:42:20 -0600

    Optimization #2

--- Postalot/Assets/Postalot/Main/Scripts/videoscript.cs
+++ Postalot/Assets/Postalot/Main/Scripts/videoscript.cs
@@ -20,8 +20,25 @@ void Start(){
     
     m_RawImage = GetComponent<RawImage>();
     videoPlayer = GetComponent<VideoPlayer>();
+    
+    //It seems the video never readies itself until it actually plays.
+    //So I'll ready it every time an object is made..?
+    videoPlayer.Prepare();
 
-    //m_RawImage.texture = videoPlayer.texture;
+    StartCoroutine("isReady");
+
+}
+
+public IEnumerator isReady() {
+
+    while(!videoPlayer.isPrepared) {
+    Debug.Log("Not ready!");
+    yield return null;
+    }
+
+    videoPlayer.frame = 0;
+
+    m_RawImage.texture = videoPlayer.texture;
 
 }
 
@@ -29,11 +46,17 @@ void Update(){
 
     //Constantly update the video texture
 
+    if(videoPlayer.isPrepared) {
+
+    if(videoPlayer.isPlaying) 
     m_RawImage.texture = videoPlayer.texture;
 
+    
+
+    //This detection function should happen once- not in every single video block..
+    // I should attach it to whatever "seen" marker I have yet to make. //Seen marker will probably just be onLoad.
 
     //Function to ensure video pauses plays off camera and on
-
 
         Vector3 screenCenter = new Vector3(Screen.width / 2, Screen.height / 2, Camera.main.transform.position.z);
         //Debug.Log("screenCenter "+screenCenter);
@@ -52,15 +75,18 @@ void Update(){
  
         float distY = Vector3.Distance(new Vector3(0f, Screen.height / 2, 0f), new Vector3(0f, goscreen.y, 0f));
         //Debug.Log("distY " + distY);
- 
+    
+
+        
         if(distX > Screen.width / 2 || distY > Screen.height / 2)
         {
             videoPlayer.Pause();
+        
         }
         else
             videoPlayer.Play();
 
-
+    }
 
 }
 
